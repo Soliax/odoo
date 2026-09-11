e8e29fe64c7f87cddd833ec1c7546e7678901515
Lallemand <anton.lallemand@gmail.com>
2026-09-11 10:37:19 +0200

    fix(website_member): split special corners and move category seals onto name plate

--- custom_addons/website_member/__manifest__.py
+++ custom_addons/website_member/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Members",
-    "version": "19.0.2.9.8",
+    "version": "19.0.2.10.4",
     "category": "Website",
     "summary": "Member directory TCG cards + event participant website blocks",
     "description": """
